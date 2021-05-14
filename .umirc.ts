d53f0fc677d4e1836cdf3b194febeee3ebddb516
linshengming <1457815431@qq.com>
2021-05-14 12:13:03 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     nodeModulesTransform: {
         type: 'none',
     },
+    favicon: 'favicon.ico',
     routes: [
         // { path: '/', component: '@/pages/index' },
         {   
